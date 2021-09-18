1b32f86247a464498555cf8afb51631c1a53e9d9
Francis Nana-Dabankah <francis@Franciss-MacBook-Pro.local>
2021-09-18 10:00:33 +0100

    added consumer code

---
+++ src/main/java/com/javakafka/consumer/MyTopicConsumer.java
@@ -0,0 +1,25 @@
+package com.javakafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MyTopicConsumer {
+
+    private final List<String> messages = new ArrayList<>();
+
+    @KafkaListener(topics = "myTopic", groupId = "kafka-sandbox")
+    public void listen(String message) {
+        synchronized (messages) {
+            messages.add(message);
+        }
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+}
--- src/main/java/com/javakafka/controller/KafkaController.java
+++ src/main/java/com/javakafka/controller/KafkaController.java
@@ -1,5 +1,6 @@
 package com.javakafka.controller;
 
+import com.javakafka.consumer.MyTopicConsumer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +12,21 @@ import java.util.List;
 public class KafkaController {
 
     private KafkaTemplate<String, String> template;
+    private MyTopicConsumer myTopicConsumer;
 
-    public KafkaController(KafkaTemplate<String, String> template) {
+    public KafkaController(KafkaTemplate<String, String> template, MyTopicConsumer myTopicConsumer) {
         this.template = template;
+        this.myTopicConsumer = myTopicConsumer;
     }
 
     @GetMapping("/kafka/produce")
     public void produce(@RequestParam String message) {
         template.send("myTopic", message);
+    }
+
+    @GetMapping("/kafka/messages")
+    public List<String> getMessages() {
+        return myTopicConsumer.getMessages();
     }
 
 }
